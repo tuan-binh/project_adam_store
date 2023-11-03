c6adb7ce34950a93653e971f5fe991efd6bdbf7e
tuan-binh <binh1892001@gmail.com>
2023-11-03 10:06:00 +0700

    update

--- front_end/src/components/banner/Banner.jsx
+++ front_end/src/components/banner/Banner.jsx
@@ -1,9 +1,11 @@
 function Banner() {
   return (
     <div
-      className="mt-10 banner bg-fixed bg-no-repeat bg-cover h-80 overflow-auto p-8 bg-center"
+      className="relative mt-10 banner bg-fixed bg-no-repeat bg-cover h-80 overflow-auto p-8 bg-center"
       style={{ backgroundImage: 'url("banner.webp")' }}
-    ></div>
+    >
+      <img src="logo_adam.png" alt="" className="absolute right-1/2 bottom-1/2 translate-y-1/2 translate-x-1/2 w-72" />
+    </div>
   );
 }
 
--- front_end/src/pages/users/home/UserHome.jsx
+++ front_end/src/pages/users/home/UserHome.jsx
@@ -1,4 +1,5 @@
 import Banner from "../../../components/banner/Banner";
+import Button from "@mui/material/Button";
 import SearchIcon from '@mui/icons-material/Search';
 import TextField from "@mui/material/TextField";
 
@@ -8,7 +9,7 @@ function UserHome() {
       <Banner />
       <main className="mx-48 my-20">
         {/* search */}
-        <div className="search flex justify-center">
+        <section className="search flex justify-center">
           <div className="relative" style={{ width: "70%" }}>
             <TextField
               fullWidth
@@ -18,15 +19,79 @@ function UserHome() {
             />
             <SearchIcon sx={{ fontSize: '35px' }} className="absolute right-3 bottom-1/2 translate-y-1/2 hover:cursor-pointer" />
           </div>
+        </section>
+        {/* grid category */}\
+        <section>
+          <h2 className="text-center text-3xl pt-3">Categories</h2>
+          <div className="flex gap-5 py-20">
+            <div className="relative">
+              <div className="bg-orange-600 w-52 h-96 overflow-hidden">
+                <img src="banner_left.webp" alt="" className="hover:scale-110 transition-all duration-500 h-full object-cover" />
+              </div>
+              <div className="absolute -rotate-90 text-white right-1/2 bottom-1/2 translate-x-1/2 translate-y-1/2 text-7xl">ADAM</div>
+            </div>
+            <div className="flex-1 py-5">
+              {/* content category */}
+              <div className="grid grid-cols-3 gap-4">
+                <div className="h-40 relative rounded-lg bg-fixed bg-no-repeat bg-cover bg-center" style={{ backgroundImage: 'url("banner.webp")' }}>
+                  <div className="absolute bottom-2 right-4 text-4xl font-semibold text-white">NỘI DUNG</div>
+                </div>
+                <div className="h-40 relative rounded-lg bg-fixed bg-no-repeat bg-cover bg-center" style={{ backgroundImage: 'url("banner.webp")' }}>
+                  <div className="absolute bottom-2 right-4 text-4xl font-semibold text-white">NỘI DUNG</div>
+                </div>
+                <div className="h-40 relative rounded-lg bg-fixed bg-no-repeat bg-cover bg-center" style={{ backgroundImage: 'url("banner.webp")' }}>
+                  <div className="absolute bottom-2 right-4 text-4xl font-semibold text-white">NỘI DUNG</div>
+                </div>
+                <div className="h-40 relative rounded-lg bg-fixed bg-no-repeat bg-cover bg-center" style={{ backgroundImage: 'url("banner.webp")' }}>
+                  <div className="absolute bottom-2 right-4 text-4xl font-semibold text-white">NỘI DUNG</div>
+                </div>
+              </div>
+            </div>
+            <div className="relative">
+              <div className="bg-orange-600 w-52 h-96 overflow-hidden">
+                <img src="banner_right.webp" alt="" className="hover:scale-125 transition-all duration-500 h-full object-cover" />
+              </div>
+              <div className="absolute rotate-90 text-white right-1/2 bottom-1/2 translate-x-1/2 translate-y-1/2 text-7xl">STORE</div>
+            </div>
+          </div>
+        </section>
 
-        </div>
-        {/* grid category */}
-        <div className="grid grid-rows-2 grid-cols-3 gap-4 p-20">
-          <div className="row-span-2 bg-red-600">suit</div>
-          <div className="col-span-1 bg-orange-600">T shirt</div>
-          <div className="col-span-1 bg-cyan-600">shirt</div>
-          <div className="row-span-1 col-span-2 bg-blue-600">shoe</div>
-        </div>
+
+        {/* nội dung những người nổi tiếng */}
+        <section>
+          <h2 className="text-center text-3xl">NGƯỜI NỔI TIẾNG</h2>
+          <div className="idol flex gap-5 p-20">
+            <div>
+              <img src="https://theme.hstatic.net/1000333436/1001040510/14/htesti_img_1_large.jpg?v=142" style={{ filter: 'blur(0px) grayscale(100%) sepia(0)' }} alt="" />
+              <h3 className="text-center py-3 uppercase text-lg font-semibold">Ca Sĩ Lý Hải</h3>
+            </div>
+            <div>
+              <img src="https://theme.hstatic.net/1000333436/1001040510/14/htesti_img_2_large.jpg?v=142" style={{ filter: 'blur(0px) grayscale(100%) sepia(0)' }} alt="" />
+              <h3 className="text-center py-3 uppercase text-lg font-semibold">Ca Sĩ Ngô Kiến Huy</h3>
+            </div>
+            <div>
+              <img src="https://theme.hstatic.net/1000333436/1001040510/14/htesti_img_3_large.jpg?v=142" style={{ filter: 'blur(0px) grayscale(100%) sepia(0)' }} alt="" />
+              <h3 className="text-center py-3 uppercase text-lg font-semibold">Nghệ Sĩ Xuân Bắc</h3>
+            </div>
+            <div>
+              <img src="https://theme.hstatic.net/1000333436/1001040510/14/htesti_img_4_large.jpg?v=142" style={{ filter: 'blur(0px) grayscale(100%) sepia(0)' }} alt="" />
+              <h3 className="text-center py-3 uppercase text-lg font-semibold">Ca Sĩ Quân AP</h3>
+            </div>
+          </div>
+        </section>
+        {/* đặt làm */}
+        <section className="flex justify-center ">
+          <div className="w-1/2">
+            <h2 className="text-center text-3xl py-5">CONTACT</h2>
+            <div className="flex flex-col gap-5">
+              <TextField fullWidth id="outlined-basic" label="EMAIL" variant="outlined" />
+              <TextField fullWidth id="outlined-basic" label="PHONE" variant="outlined" />
+            </div>
+            <div className="action flex justify-center py-5">
+              <Button variant="contained" sx={{ minWidth: '200px' }}>SEND</Button>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
